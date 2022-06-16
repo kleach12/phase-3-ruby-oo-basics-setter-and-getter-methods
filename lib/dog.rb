337d9c8dbb7fb42fbe45c5c20289078b38409eed
Kale Leach <kleach1212@gmail.com>
2022-06-16 10:03:44 -0700

    commit tree 88b676da9dd23e991ebb5ef3f079a30118c9de1b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
+  def name=(name)
+    @name = name
+  end
+
+  def name 
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 
 end
+
+
+bambi = Dog.new
+bambi.name = "Bambi"
+bambi.breed = "chihuahua"
+puts bambi.name
+puts bambi.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end
 
 end
+
+kale = Person.new
+kale.name = "Kale"
+kale.job = "Call Center"
+puts kale.name
+puts kale.job
